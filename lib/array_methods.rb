cc8bd3115d06f7f89dbfc8e98dec2c8ed542158e
Adeja Lendor <alendor6@gmail.com>
2019-08-28 12:23:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,19 +1,11 @@
 def find_element_index(array, value_to_find)
-  index = 0 
-  array.length.times do |index|
-    if array[index] == value_to_find
-      return index
-  end
-end
-nil
+
 end
 
 def find_max_value(array)
- number = array.max 
- number
+ array.max
 end
 
 def find_min_value(array)
-  number = array.min 
-  number
+  array.min
 end
